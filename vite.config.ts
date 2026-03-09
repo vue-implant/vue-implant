98e94582d2362cbabd35e756e58d9307551cfef1
流光墨佰 <1561317465@qq.com>
2026-03-09 21:45:57 +0800

    feat: update package.json and vite.config.ts for library build configuration and type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: "VueInjector",
+      fileName: 'vue-injector'
+    },
+    rolldownOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['vue']
   },
-  plugins: [vue()],
+  plugins: [vue(), dts({
+    rollupTypes: true,
+    tsconfigPath: resolve(__dirname, 'tsconfig.app.json'),
+    outDir: 'dist/types',
+
+    exclude: ['src/__test__/**/*', 'src/**/*.test.ts'],
+    include: ['src/**/*.ts']
+  })],
   test: {
     silent: true,
     environment: 'jsdom',
